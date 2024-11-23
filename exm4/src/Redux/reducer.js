556a3ff6a88f8653abae0249eb70c9c3d8916faf
Alishan rwbn <alishanshaikh9890@gmail.com>
2024-11-23 18:51:23 +0530

    .

--- exm4/src/Redux/reducer.js
+++ exm4/src/Redux/reducer.js
@@ -2,6 +2,7 @@ import { INC_COUNT, DEC_COUNT } from "./action";
 
 
 export const reducer = (store={count : 0}, {type, payload}) =>{
+    console.log("in reducer: action dispatches " ,  {type, payload});
     switch(type){
         case INC_COUNT:
             return { count: store.count + payload};
--- exm4/src/Redux/store.js
+++ exm4/src/Redux/store.js
@@ -1,13 +1,21 @@
-import {legacy_createStore as createStore} from "redux"
+import {applyMiddleware, legacy_createStore as createStore} from "redux";
 
 import { reducer } from "./reducer";
 
-const loggermiddleware = (store) => (next) => (action) => {
+const loggerMiddleware = (store) => (next) => (action) => {
     console.log("action dispatch is ", action);
     next(action);
 }
 
+const middleware  = applyMiddleware(loggerMiddleware);
+
 export const store  = createStore(
     reducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    middleware
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
+
+
+store.subscribe(() => {
+    console.log("current state", store.getState());
+});
