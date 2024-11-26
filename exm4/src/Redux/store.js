b9ebe218385e9d404e349c1b92837aeae9a0cc8b
Alishan Shaikh <alishanshaikh9890@gmail.com>
2024-11-26 09:15:43 +0530

    in progress

--- exm4/src/Redux/store.js
+++ exm4/src/Redux/store.js
@@ -2,16 +2,21 @@ import {applyMiddleware, legacy_createStore as createStore} from "redux";
 
 import { reducer } from "./reducer";
 
-const loggerMiddleware = (store) => (next) => (action) => {
-    console.log("action dispatch is ", action);
+const loggerMiddleware1 = (store) => (next) => (action) => {
+    console.log("1. action dispatch is ", action);
     next(action);
 }
 
-const middleware  = applyMiddleware(loggerMiddleware);
+const loggerMiddleware2 = (store) => (next) => (action) => {
+    console.log("2. action dispatch is ", action);
+    action.payload = 3;
+    next(action);
+}
+
 
 export const store  = createStore(
     reducer,
-    middleware
+   applyMiddleware(loggerMiddleware1, loggerMiddleware2)
     // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
